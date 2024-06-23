efd9462456722d34dad253b6023fe7acff0de89f
Oriol <0m1c20n@gmail.com>
2024-06-23 13:43:54 +0200

    Disable form

--- src/components/Sections/Contact/ContactForm.tsx
+++ src/components/Sections/Contact/ContactForm.tsx
@@ -33,23 +33,23 @@ const ContactForm: FC = memo(() => {
     async (event: React.FormEvent<HTMLFormElement>) => {
       event.preventDefault();
 
-      const res = await fetch("/api/sendgrid", {
-        body: JSON.stringify({
-          email: data.email,
-          name: data.name,
-          message: data.message,
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-        method: "POST",
-      });
+      // const res = await fetch("/api/sendgrid", {
+      //   body: JSON.stringify({
+      //     email: data.email,
+      //     name: data.name,
+      //     message: data.message,
+      //   }),
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //   },
+      //   method: "POST",
+      // });
 
-      const {error} = await res.json();
-      if (error) {
-        console.log(error);
-        return;
-      }
+      // const {error} = await res.json();
+      // if (error) {
+      //   console.log(error);
+      //   return;
+      // }
       console.log('Data sent: ', data);
     },
     [data],
